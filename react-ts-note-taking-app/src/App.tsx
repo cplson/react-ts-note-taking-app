70d5550e3772dd48fe1f580567b7beb02af975d1
James Jacobsen <edjcjacobsen@gmail.com>
2023-06-06 21:20:07 -0500

    Completed basic routing that will be required for the app using react-router-dom

--- react-ts-note-taking-app/src/App.tsx
+++ react-ts-note-taking-app/src/App.tsx
@@ -1,7 +1,27 @@
 // import { useState } from 'react'
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
-  return <h1>Hi</h1>
+  return (
+    // Wrap contents of the App in a bootstrap Container and give it 
+    // top and bottom margin of 4px
+    <Container className="my-4">
+    {/* Define all routes using react-router-dom */}
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<h1>New</h1>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        {/* For any endpoint that does not match a defined 
+          route, use <Navigate> to redirect to home */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  )
 }
 
 export default App
